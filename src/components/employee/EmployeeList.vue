9656b260ec7f8ab0a55f405d34bb17a965bfcc95
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 18:55:12 +0330

    Use EmployeeListItem to render employees in list

---
+++ src/components/employee/EmployeeList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import EmployeeListItem from '@/components/employee/EmployeeListItem.vue';
+import useEmployeeStore from '@/stores/employee';
+
+const employeeStore = useEmployeeStore();
+</script>
+
+<template>
+    <ol class="flex flex-col mt-5">
+        <EmployeeListItem
+            v-for="employee of employeeStore.employees"
+            :key="employee.id"
+            :employee
+        />
+    </ol>
+</template>
