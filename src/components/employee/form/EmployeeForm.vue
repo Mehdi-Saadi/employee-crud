5c172b1d22af92cf8193a9e8f6661dc8cefd64fa
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 21:23:52 +0330

    employee/form/EmployeeFormField.vue -> form/FormField.vue

--- src/components/employee/form/EmployeeForm.vue
+++ src/components/employee/form/EmployeeForm.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import ButtonComponent from '@/components/buttons/ButtonComponent.vue';
-import EmployeeFormField from '@/components/employee/form/EmployeeFormField.vue';
+import FormField from '@/components/form/FormField.vue';
 import type { EmployeeToAdd } from '@/types/employee';
 import { ref } from 'vue';
 
@@ -31,46 +31,46 @@ const submit = (): void => {
 
 <template>
     <div class="flex flex-col space-y-5 p-5 border rounded mt-5">
-            <!-- employee data -->
-            <div class="grid grid-cols-2 gap-5">
-                <EmployeeFormField
-                    :autofocus="true"
-                    v-model="form.firstName"
-                    id="name"
-                    label="نام"
-                    type="text"
-                />
-                <EmployeeFormField
-                    v-model="form.lastName"
-                    label="نام خانوادگی"
-                    id="last-name"
-                    type="text"
-                />
-                <EmployeeFormField
-                    v-model="form.dateOfBirth"
-                    label="تاریخ تولد"
-                    id="date-of-birth"
-                    type="date"
-                />
-                <EmployeeFormField
-                    v-model="form.email"
-                    label="ایمیل"
-                    id="email"
-                    type="email"
-                />
-            </div>
-            <!-- buttons -->
-            <div class="flex items-center justify-between">
-                <ButtonComponent
-                    @click="submit()"
-                    class="text-white bg-green-700 hover:bg-green-800"
-                    title="افزودن"
-                />
-                <ButtonComponent
-                    @click="emit('close')"
-                    class="text-white bg-gray-500 hover:bg-gray-600"
-                    title="انصراف"
-                />
-            </div>
+        <!-- employee data -->
+        <div class="grid grid-cols-2 gap-5">
+            <FormField
+                :autofocus="true"
+                v-model="form.firstName"
+                id="name"
+                label="نام"
+                type="text"
+            />
+            <FormField
+                v-model="form.lastName"
+                label="نام خانوادگی"
+                id="last-name"
+                type="text"
+            />
+            <FormField
+                v-model="form.dateOfBirth"
+                label="تاریخ تولد"
+                id="date-of-birth"
+                type="date"
+            />
+            <FormField
+                v-model="form.email"
+                label="ایمیل"
+                id="email"
+                type="email"
+            />
         </div>
+        <!-- buttons -->
+        <div class="flex items-center justify-between">
+            <ButtonComponent
+                @click="submit()"
+                class="text-white bg-green-700 hover:bg-green-800"
+                title="افزودن"
+            />
+            <ButtonComponent
+                @click="emit('close')"
+                class="text-white bg-gray-500 hover:bg-gray-600"
+                title="انصراف"
+            />
+        </div>
+    </div>
 </template>
