ff98ddf5ba9e08f9fe20a835e3a44eb3e6fd7f3e
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 17:47:38 +0330

    Set axios defaults, Use env vars

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
+
+// set axios defaults
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 const app = createApp(App);
 
