37048fb47c7e4def4dc4d84c2de7d4cf35911d8b
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 18:41:39 +0330

    Use route title for heading

--- src/layouts/EmployeeLayout.vue
+++ src/layouts/EmployeeLayout.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
+
 <template>
-    <div class="w-full max-w-screen-md mx-auto">
+    <div class="w-full max-w-screen-md mx-auto pt-20 flex flex-col">
+        <h1 class="text-2xl px-5">
+            {{ route.meta.title }}
+        </h1>
         <slot />
     </div>
 </template>
