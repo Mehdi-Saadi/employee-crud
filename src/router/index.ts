7b65c004ade570fe1ec135d3afb720e7edc3a197
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 17:40:24 +0330

    EmployeesView -> EmployeeListView, Use EmployeeLayout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import EmployeesView from '@/views/EmployeesView.vue';
+import EmployeeListView from '@/views/EmployeeListView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
         {
             path: '/employees',
             name: 'employee.list',
-            component: EmployeesView,
+            component: EmployeeListView,
         },
     ],
 });
---
+++ src/views/EmployeeListView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import EmployeeLayout from '@/layouts/EmployeeLayout.vue';
+
+</script>
+
+<template>
+    <EmployeeLayout>
+        کارمندان
+    </EmployeeLayout>
+</template>
