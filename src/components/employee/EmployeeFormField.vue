38fdd418c04a69ca79f3f9da0be4c67802c4fe7e
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 19:23:50 +0330

    Add form field

---
+++ src/components/employee/EmployeeFormField.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        label: string;
+        id: string;
+        autofocus?: boolean;
+    }>(),
+    {
+        autofocus: false,
+    }
+);
+
+const model = defineModel();
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <label
+            :for="id"
+            class="px-3"
+        >
+            {{ label }}
+        </label>
+        <input
+            :autofocus
+            :id
+            :name="id"
+            v-model="model"
+            class="border border-gray-300 focus:ring-0 focus:border-gray-400 rounded"
+            type="text"
+        />
+    </div>
+</template>
