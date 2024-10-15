b2d1f25a5877e52fd88fddc1723d6ca18b205b3f
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 18:55:42 +0330

    Show employees after fetch

--- src/views/EmployeeListView.vue
+++ src/views/EmployeeListView.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+import EmployeeList from '@/components/employee/EmployeeList.vue';
 import EmployeeLayout from '@/layouts/EmployeeLayout.vue';
 import useEmployeeStore from '@/stores/employee';
 
-const { fetchEmployees } = useEmployeeStore();
+const employeeStore = useEmployeeStore();
+const { fetchEmployees } = employeeStore;
 
 fetchEmployees();
 </script>
 
 <template>
     <EmployeeLayout>
-        کارمندان
+        <EmployeeList v-if="employeeStore.employees.length" />
+        <span v-else> در حال بارگیری... </span>
     </EmployeeLayout>
 </template>
