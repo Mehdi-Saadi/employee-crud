9a5beff6b84e0e04b3735ef88e7a453a8223cc25
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 20:47:16 +0330

    Implement add employee functionality

--- src/components/employee/form/EmployeeAddForm.vue
+++ src/components/employee/form/EmployeeAddForm.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
 import ButtonComponent from '@/components/buttons/ButtonComponent.vue';
 import EmployeeFormField from '@/components/employee/form/EmployeeFormField.vue';
+import useEmployeeStore from '@/stores/employee';
+import type { EmployeeToAdd } from '@/types/employee';
+import { cloneDeep } from 'lodash';
 import { ref } from 'vue';
 
+const { addEmployee } = useEmployeeStore();
 const showFrom = ref<boolean>(false);
+
+const defaultFormValue: EmployeeToAdd = {
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
+    family: [],
+};
+
+const form = ref<EmployeeToAdd>(cloneDeep(defaultFormValue));
+
+const submit = (): void => {
+    addEmployee(form.value);
+
+    // reset form fields to default
+    form.value = cloneDeep(defaultFormValue);
+};
 </script>
 
 <template>
@@ -17,21 +38,25 @@ const showFrom = ref<boolean>(false);
             <div class="grid grid-cols-2 gap-5">
                 <EmployeeFormField
                     :autofocus="true"
+                    v-model="form.firstName"
                     id="name"
                     label="نام"
                     type="text"
                 />
                 <EmployeeFormField
+                    v-model="form.lastName"
                     label="نام خانوادگی"
                     id="last-name"
                     type="text"
                 />
                 <EmployeeFormField
+                    v-model="form.dateOfBirth"
                     label="تاریخ تولد"
                     id="date-of-birth"
                     type="date"
                 />
                 <EmployeeFormField
+                    v-model="form.email"
                     label="ایمیل"
                     id="email"
                     type="email"
@@ -40,6 +65,7 @@ const showFrom = ref<boolean>(false);
             <!-- buttons -->
             <div class="flex items-center justify-between">
                 <ButtonComponent
+                    @click="submit()"
                     class="text-white bg-green-700 hover:bg-green-800"
                     title="افزودن"
                 />
