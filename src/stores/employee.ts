c571dcc7bd906e774b803989561d79aae6a0cb5c
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 18:21:31 +0330

    Add fetchEmployees action

--- src/stores/employee.ts
+++ src/stores/employee.ts
@@ -1,12 +1,24 @@
-import type { Employee } from '@/types/employee';
+import type { EmployeeBrief } from '@/types/employee';
+import axios from 'axios';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 const useEmployeeStore = defineStore('employee', () => {
-    const employees = ref<Employee[]>([]);
+    const employees = ref<EmployeeBrief[]>([]);
+
+    const fetchEmployees = async (): Promise<void> => {
+        try {
+            const response = await axios.get('/employee');
+
+            employees.value = response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return {
         employees,
+        fetchEmployees,
     };
 });
 
