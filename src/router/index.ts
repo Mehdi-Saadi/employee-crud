097a77b2d6affa85201af47553355551bb203b7b
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 16:31:15 +0330

    Remove unused route, Add employees route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import AboutView from '../views/AboutView.vue';
+import EmployeesView from '@/views/EmployeesView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
-            name: 'home',
-            component: AboutView,
+            path: '/employees',
+            name: 'employee.list',
+            component: EmployeesView,
         },
     ],
 });
--- src/views/AboutView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>فونت وزیر</h1>
-</template>
