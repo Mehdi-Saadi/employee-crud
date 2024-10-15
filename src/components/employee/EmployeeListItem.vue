36455cf7ec2ed16202c5f18719e501ca01a8b795
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 18:53:52 +0330

    Add EmployeeListItem component

---
+++ src/components/employee/EmployeeListItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ChevronDownIcon from '@/components/icons/ChevronDownIcon.vue';
+import type { EmployeeBrief } from '@/types/employee';
+
+defineProps<{
+    employee: EmployeeBrief;
+}>();
+</script>
+
+<template>
+    <li class="flex items-center justify-between border h-14 px-5 cursor-pointer">
+        <span>
+            {{ employee.firstName }}
+        </span>
+        <ChevronDownIcon class="size-4" />
+    </li>
+</template>
