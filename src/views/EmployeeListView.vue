dce8a420457e230eae41f5cdb44064856a2fcc6f
mehdi <mehdi.0.saadi@gmail.com>
2024-10-15 18:22:05 +0330

    Use fetchEmployees

--- src/views/EmployeeListView.vue
+++ src/views/EmployeeListView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import EmployeeLayout from '@/layouts/EmployeeLayout.vue';
+import useEmployeeStore from '@/stores/employee';
 
+const { fetchEmployees } = useEmployeeStore();
+
+fetchEmployees();
 </script>
 
 <template>
