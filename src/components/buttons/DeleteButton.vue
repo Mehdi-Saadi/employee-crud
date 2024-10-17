12a55965fdabaa93a91892b00fee4d0b92bce16a
mehdi <mehdi.0.saadi@gmail.com>
2024-10-17 17:46:56 +0330

    Use spin icon on disabled

--- src/components/buttons/DeleteButton.vue
+++ src/components/buttons/DeleteButton.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+import SpinIcon from '@/components/icons/SpinIcon.vue';
 import TrashIcon from '@/components/icons/TrashIcon.vue';
+
+defineProps<{
+    disabled?: boolean;
+}>();
 </script>
 
 <template>
     <button
-        class="size-10 bg-red-600 hover:bg-red-800 rounded flex items-center justify-center ms-auto me-2"
+        class="size-10 bg-red-600 hover:bg-red-800 rounded flex items-center justify-center ms-auto me-2 disabled:opacity-45 text-white"
         type="button"
     >
-        <TrashIcon class="size-6 text-white" />
+        <SpinIcon
+            v-if="disabled"
+            class="animate-spin size-5"
+        />
+        <TrashIcon
+            v-else
+            class="size-6"
+        />
     </button>
 </template>
