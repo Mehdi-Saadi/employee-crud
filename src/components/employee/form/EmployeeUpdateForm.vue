fd683f8564ffa2565ab1ab8a7890a4daeeb25fc9
mehdi <mehdi.0.saadi@gmail.com>
2024-10-17 17:40:10 +0330

    Add update form validation

--- src/components/employee/form/EmployeeUpdateForm.vue
+++ src/components/employee/form/EmployeeUpdateForm.vue
@@ -2,6 +2,7 @@
 import EmployeeForm from '@/components/employee/form/EmployeeForm.vue';
 import useEmployeeStore from '@/stores/employee';
 import type { Employee, EmployeeToAdd } from '@/types/employee';
+import { validateEmployee } from '@/scripts/validation';
 import { toastAlert } from '@/scripts/sweetalert';
 import { cloneDeep } from 'lodash';
 
@@ -13,12 +14,14 @@ const { updateEmployee } = useEmployeeStore();
 const { id, ...defaultEmployeeValue } = cloneDeep(props.employee);
 
 const submit = async (employee: EmployeeToAdd): Promise<void> => {
-    await updateEmployee({
-        id: id,
-        ...cloneDeep(employee),
-    });
+    if (validateEmployee(employee)) {
+        await updateEmployee({
+            id: id,
+            ...cloneDeep(employee),
+        });
 
-    toastAlert('success', 'اطلاعات بروز شد');
+        toastAlert('success', 'اطلاعات بروز شد');
+    }
 };
 </script>
 
