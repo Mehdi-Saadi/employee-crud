60b73d5dd797fbe63d727cd84369000aa26bbb7e
mehdi <mehdi.0.saadi@gmail.com>
2024-10-17 17:33:25 +0330

    Use spin icon for disabled buttons

--- src/components/buttons/ButtonComponent.vue
+++ src/components/buttons/ButtonComponent.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
+import SpinIcon from '@/components/icons/SpinIcon.vue';
+
 defineProps<{
     title: string;
+    disabled?: boolean;
 }>();
 </script>
 
 <template>
     <button
-        class="rounded max-w-max px-5 py-2 transition duration-200 disabled:opacity-45"
+        class="rounded max-w-max px-5 py-2 transition duration-200 disabled:opacity-45 flex items-center justify-center"
         type="button"
+        :disabled
     >
-        {{ title }}
+        <SpinIcon
+            v-if="disabled"
+            class="animate-spin size-5"
+        />
+        <span v-else>
+            {{ title }}
+        </span>
     </button>
 </template>
