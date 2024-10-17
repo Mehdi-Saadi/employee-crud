d2d8741c2f1e34f7ee001d034b3d23a9cbc3a62a
mehdi <mehdi.0.saadi@gmail.com>
2024-10-17 16:20:03 +0330

    Set isLoading state

--- src/stores/employee.ts
+++ src/stores/employee.ts
@@ -1,22 +1,52 @@
 import type { Employee, EmployeeBrief, EmployeeToAdd } from '@/types/employee';
-import axios from 'axios';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import axios from 'axios';
+
+type IsLoadingParamKeys =
+    | 'fetchEmployees'
+    | 'getEmployeeDetails'
+    | 'addEmployee'
+    | 'updateEmployee'
+    | 'deleteEmployee';
+
+type IsLoading = {
+    [K in IsLoadingParamKeys]: boolean;
+};
 
 const useEmployeeStore = defineStore('employee', () => {
     const employees = ref<EmployeeBrief[]>([]);
+    const isLoading = ref<IsLoading>({
+        fetchEmployees: false,
+        getEmployeeDetails: false,
+        addEmployee: false,
+        updateEmployee: false,
+        deleteEmployee: false,
+    });
+
+    const setLoading = (key: IsLoadingParamKeys, status: boolean): void => {
+        isLoading.value[key] = status;
+    };
+    const setLoadingTrue = (key: IsLoadingParamKeys): void => setLoading(key, true);
+    const setLoadingFalse = (key: IsLoadingParamKeys): void => setLoading(key, false);
 
     const fetchEmployees = async (): Promise<void> => {
+        setLoadingTrue('fetchEmployees');
+
         try {
             const response = await axios.get('/employee');
 
             employees.value = response.data;
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoadingFalse('fetchEmployees');
         }
     };
 
     const getEmployeeDetails = async (employeeId: Employee['id']): Promise<Employee | null> => {
+        setLoadingTrue('getEmployeeDetails');
+
         try {
             const response = await axios.get(`/employee/${employeeId}`);
 
@@ -25,10 +55,14 @@ const useEmployeeStore = defineStore('employee', () => {
             console.error(error);
 
             return null;
+        } finally {
+            setLoadingFalse('getEmployeeDetails');
         }
     };
 
     const addEmployee = async (employee: EmployeeToAdd): Promise<void> => {
+        setLoadingTrue('addEmployee');
+
         try {
             const response = await axios.post('/employee', employee);
 
@@ -39,10 +73,14 @@ const useEmployeeStore = defineStore('employee', () => {
             });
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoadingFalse('addEmployee');
         }
     };
 
     const updateEmployee = async (employee: Employee): Promise<void> => {
+        setLoadingTrue('updateEmployee');
+
         try {
             const response = await axios.put(`/employee/${employee.id}`, employee);
 
@@ -56,10 +94,14 @@ const useEmployeeStore = defineStore('employee', () => {
             }
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoadingFalse('updateEmployee');
         }
     };
 
     const deleteEmployee = async (employeeId: Employee['id']): Promise<void> => {
+        setLoadingTrue('deleteEmployee');
+
         try {
             await axios.delete(`/employee/${employeeId}`);
 
@@ -72,11 +114,14 @@ const useEmployeeStore = defineStore('employee', () => {
             }
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoadingFalse('deleteEmployee');
         }
     };
 
     return {
         employees,
+        isLoading,
         fetchEmployees,
         getEmployeeDetails,
         addEmployee,
