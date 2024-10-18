52d8e21e793a73d224f3e6095683e16fc9441337
mehdi <mehdi.0.saadi@gmail.com>
2024-10-18 13:10:12 +0330

    Add type prop

--- src/components/buttons/ButtonComponent.vue
+++ src/components/buttons/ButtonComponent.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import SpinIcon from '@/components/icons/SpinIcon.vue';
 
-defineProps<{
-    title: string;
-    disabled?: boolean;
-}>();
+withDefaults(
+    defineProps<{
+        title: string;
+        disabled?: boolean;
+        type?: 'button' | 'submit'
+    }>(),
+    {
+        type: 'button'
+    }
+);
 </script>
 
 <template>
     <button
         class="rounded max-w-max px-5 py-2 transition duration-200 disabled:opacity-45 flex items-center justify-center"
-        type="button"
+        :type
         :disabled
     >
         <SpinIcon
